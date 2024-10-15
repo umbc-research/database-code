0464f722ee7d6f6bdd6f570f2d28b42a3b781ee7
navyassassin <mb17@umbc.edu>
2024-10-15 06:33:40 -0700

    adding first commit

---
+++ pymysql_test.py
@@ -0,0 +1,33 @@
+#!/usr/home/mb17/database_work/backend/bin/python3
+
+import pymysql.cursors
+"""
+Assumes the following table has been created already:
+    CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) COLLATE utf8_bin NOT NULL,
+    `password` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=1 ;
+"""
+def main():
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='IA2&gtgr7TOxR4hRM5wwTm#%2m',
+                                 database='obsstor_test',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor) #a cursor is just a object used to interact with the database, this particular cursor returns results as a dictionary
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `test` (`email`, `password`) VALUES (%s, %s)"
+            cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+        connection.commit()
+        with connection.cursor() as cursor:
+            sql = "SELECT `id`, `password` FROM `test` WHERE `email`=%s"
+            cursor.execute(sql, ('webmaster@python.org',))
+            result = cursor.fetchone()
+            print(result)
+
+if __name__ == "__main__":
+    main()
